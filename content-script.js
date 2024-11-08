e4b8b6b9e2b551006dd0da70843ba9231c54a687
patrr2 <snailmail>
2024-11-09 00:46:45 +0200

    add tippy.js

--- content-script.js
+++ content-script.js
@@ -6,6 +6,16 @@ const processJobPosting = () => {
 }
 
 window.addEventListener('load', () => {
+    let scripttag1 = document.createElement('script')
+    scripttag1.src = chrome.runtime.getURL('./lib/popper.min.js')
+    document.head.appendChild(scripttag1)
+
+    let scripttag2 = document.createElement('script')
+    scripttag2.src = chrome.runtime.getURL('./lib/tippy-bundle.umd.js')
+    document.head.appendChild(scripttag2)
+
+    console.log(scripttag1)
+    console.log(scripttag2)
     // job posting html is lazily loaded, so retry until the text is there
     const retry = setInterval(() => {
         // Unloaded job posting has innerText "About the job", which is 13 characters, but other languages might have more characters
