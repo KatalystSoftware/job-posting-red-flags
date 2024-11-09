cf6a0c473312529183ad6f63839356110601c357
Mikael Siidorow <mikael@siidorow.com>
2024-11-09 16:22:35 +0200

    import imports and asyncified suffs

--- content-script.js
+++ content-script.js
@@ -1,25 +1,7 @@
-/** @type {string | undefined | null} */
-let jobPostingHTML = null;
+/** @type {Element | undefined | null} */
+let jobPostingHTMLElement = null;
 
-const processJobPosting = () => {
-  jobPostingHTML = document.querySelector(".jobs-box__html-content")?.innerHTML;
-  console.log("HTML", jobPostingHTML);
-};
-
-window.addEventListener("load", () => {
-  let scripttag1 = document.createElement("script");
-  scripttag1.src = window.chrome.runtime.getURL("./lib/popper.min.js");
-
-  scripttag1.addEventListener("load", () => {
-    let scripttag2 = document.createElement("script");
-    scripttag2.src = window.chrome.runtime.getURL("./lib/tippy-bundle.umd.js");
-    document.head.appendChild(scripttag2);
-
-    console.log(scripttag2);
-  });
-
-  document.head.appendChild(scripttag1);
-  console.log(scripttag1);
+const loadJobPosting = () => {
   // job posting html is lazily loaded, so retry until the text is there
   const retry = setInterval(() => {
     // Unloaded job posting has innerText "About the job", which is 13 characters, but other languages might have more characters
@@ -30,4 +12,47 @@ window.addEventListener("load", () => {
       processJobPosting();
     }
   }, 200);
+};
+
+const processJobPosting = () => {
+  console.log("Processing job posting");
+  jobPostingHTMLElement = document.querySelector(".jobs-box__html-content");
+  console.log("HTML", jobPostingHTMLElement?.innerHTML);
+
+  const description = jobPostingHTMLElement?.lastChild?.textContent;
+  const firstSentence = description?.split(".")[0];
+
+  console.log("First sentence", firstSentence);
+
+  if (firstSentence) {
+    const highlight = createHighlight({ type: "note" });
+    jobPostingHTMLElement?.insertAdjacentElement("beforebegin", highlight);
+    highlight.innerText = "Lorem ipsum hello";
+
+    console.log("Highlight", highlight);
+  }
+};
+
+/**
+ *
+ * @param {{
+ *   type: "note" | "positive" | "negative",
+ * }} options
+ */
+const createHighlight = (options) => {
+  const highlight = document.createElement("span");
+  highlight.classList.add("highlight");
+  highlight.classList.add(`highlight-${options.type}`);
+  window.tippy(highlight, { content: "Testing tippy" });
+  return highlight;
+};
+
+const loadExtension = async () => {
+  await import(window.chrome.runtime.getURL("./lib/popper.min.js"));
+  await import(window.chrome.runtime.getURL("./lib/tippy-bundle.umd.js"));
+  loadJobPosting();
+};
+
+window.addEventListener("load", () => {
+  void loadExtension();
 });
